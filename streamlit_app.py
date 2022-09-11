4986884db5cb06510ffa99b1ed26edfa9ff559ce
Venkata-krishnaM <113280827+Venkata-Krishnam@users.noreply.github.com>
2022-09-11 13:37:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@
 import streamlit
 streamlit.title('my parents new healthy dinner')
 streamlit.title('Break fast menu')
-streamlit('omega & omlet & blueberry')
+streamlit.title('omega & omlet & blueberry')
